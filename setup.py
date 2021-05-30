c895202b15aa36ae55544f61ab5a4c80533f9581
Peter Rice <peter@peterrice.xyz>
2021-05-30 15:21:41 -0400

    switch to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="qbpm",
-    version="0.3",
-    url="https://git.sr.ht/~pvsr/qbpm",
-    packages=find_packages(),
-    entry_points={"console_scripts": ["qbpm = qbpm.main:main"]},
-    install_requires=["pyxdg"],
-    author="Peter Rice",
-    author_email="peter@peterrice.xyz",
-    description="qutebrowser profile manager",
-)
+setup()
